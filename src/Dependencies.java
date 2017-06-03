b538c30d2fcecf4d26e6b1ecf66fd2a94784f139
Tony <atho905@aucklanduni.ac.nz>
2017-06-03 15:34:08 +1200

    line extrations

---
+++ src/Dependencies.java
@@ -0,0 +1,18 @@
+package program;
+
+public class Dependencies {
+	
+	private boolean isValid(String line)
+	{
+		char[] l = line.toCharArray();
+		while(l[i] != '=')
+		{
+			
+		}
+		
+		while(l[i] != ';')
+		{
+			
+		}
+	}
+}
--- src/program/textExtractor.java
+++ src/program/textExtractor.java
@@ -14,6 +14,7 @@ public class textExtractor {
 		textExtractor t = new textExtractor();
 		t.parseString();
 		t.NumFor();
+	
 	}
 	
 	private void parseString()
