dd123a22f82dd78479f4b140617076fbfbf48e4e
LewayneNaidoo <lkn.17@hotmail.com>
2017-06-07 17:25:39 +1200

    Finalized + comments

--- src/program/Skewing.java
+++ src/program/Skewing.java
@@ -9,36 +9,51 @@ public class Skewing {
 	
 	public void run()
 	{
+		// saves the user's input as loop string
 		String loop = t.getLines();
+		
+		// checking variable for body or which line
 		int count = 0;
+		
+		// loop through the string that was created and checks for specific characters 
 		for (int i = 0; i<loop.length();i++){
+			
+			// every "{" means that the loop is on to the next relevant part of the user's input loop
 			if (loop.charAt(i) == '{'){
 				count++;
+				
+				// if "}" then end of loop (nothing else relevant)
 			} else if (loop.charAt(i) == '}'){
 				count = 3;
 			}
 			
+			String variableOne = t.getIteratorName(1);
+			String variableTwo = t.getIteratorName(2);
+			
+			// making relevant adjustments to the variable in order to perform the skewing algorithm
 			if (count == 1){
 				if (loop.charAt(i) == '='){
-					loop = loop.substring(0,i)+"=i+"+loop.substring(i+1);
+					loop = loop.substring(0,i)+"=" + variableOne+ "+"+loop.substring(i+1);
+					// correction of i with new elements added to string
 					i += 2;
 					
 				} else if (loop.charAt(i) == '>'){
-					loop = loop.substring(0,i)+">i+"+loop.substring(i+1);
+					loop = loop.substring(0,i)+">" + variableOne+ "+"+loop.substring(i+1);
 					i += 2;
 				} else if (loop.charAt(i) == '<'){
-					loop = loop.substring(0,i)+"<i+"+loop.substring(i+1);
+					loop = loop.substring(0,i)+"<" + variableOne+ "+"+loop.substring(i+1);
 					i += 2;
 				}
 			}
 			
 			if (count == 2){
 				if (loop.charAt(i) == ']'){
-					loop = loop.substring(0,i)+"-i]"+loop.substring(i+1);
+					loop = loop.substring(0,i)+"-" + variableOne+ "]"+loop.substring(i+1);
 					i += 2;
 				}
 			}
 		}
+		// output the loop then perform interchange
 		output = loop;
 		interchange();
 	
@@ -56,6 +71,9 @@ public class Skewing {
 		String temp1;
 		String temp2;
 		String n = "", m = "", io = "";
+		
+		String variableOne = t.getIteratorName(1);
+		String variableTwo = t.getIteratorName(2);
 		
 		for (int i = 0; i < line.length; i++) {
 
@@ -76,14 +94,14 @@ public class Skewing {
 		
 		temp1 = line[looptwo];
 		temp1 = temp1.trim();
-		temp1 = temp1.replaceFirst("i", io);
-		temp1 = temp1.replaceFirst("i", n);
+		temp1 = temp1.replaceFirst(variableOne, io);
+		temp1 = temp1.replaceFirst(variableOne, n);
 		
 		temp2 = line[loopone];
 		temp2 = "\t" + temp2;
 		temp2 = temp2.replace("=", "= max(");
-		temp2 = temp2.replaceFirst(";", ", j - " + m + ");");
-		temp2 = temp2.replace(temp2.substring(temp2.indexOf("<")+2, temp2.lastIndexOf(";")+1), "min(" + n + ", j -" + io + ");");
+		temp2 = temp2.replaceFirst(";", ", " + variableTwo +" - " + m + ");");
+		temp2 = temp2.replace(temp2.substring(temp2.indexOf("<")+2, temp2.lastIndexOf(";")+1), "min(" + n + ", " + variableTwo +" -" + io + ");");
 		
 		line[loopone] = temp1;
 		line[looptwo] = temp2;
